5af6ce64ce5a9f62eaa463c814e237542831d4ff
Crusys <crusys@lixlile.com>
2026-02-21 21:34:42 +0200

    Update by Crusys

--- sys_core.js
+++ sys_core.js
@@ -1,9 +1,9 @@
 const LATEST_VERSION = "1.0.5";
 const SYSTEM_CHANGES = [
     { module: "Console", desc: "Добавлены команды .ua, .file, .say" },
-    { module: "UI Menu", desc: "Реальный Центр обновлений v1.0.5" },
-    { module: "Ukraine", desc: "Мониторинг Днепра и всех областей" },
-    { module: "Engine", desc: "Переход на динамическую память версий" }
+    { module: "UI Menu", desc: "Авто-обновление v1.0.5 [Final]" },
+    { module: "Ukraine", desc: "Мониторинг всех областей (Днепр...)" },
+    { module: "Engine", desc: "Система авто-перезагрузки после Update" }
 ];
 
 function getLocalVersion() {
